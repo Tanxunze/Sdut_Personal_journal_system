6632156ae0cd42c4d477698ac3ad93ac6364c3d6
tanxunze <wuwuhenfeng@gmail.com>
2023-12-21 09:15:55 +0800

    init 12.21

--- src/main/java/Journal_Management_System/gui/JournalEditor.java
+++ src/main/java/Journal_Management_System/gui/JournalEditor.java
@@ -1,4 +1,5 @@
 package main.java.Journal_Management_System.gui;
 
 public class JournalEditor {
+
 }
--- src/main/java/Journal_Management_System/gui/LoginFrame.java
+++ src/main/java/Journal_Management_System/gui/LoginFrame.java
@@ -1,4 +1,89 @@
 package main.java.Journal_Management_System.gui;
 
-public class LoginFrame {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class LoginFrame extends JFrame {
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JButton btnLogin, btnRegister, btnReset;
+    private JLabel lblAvatar;
+
+    public LoginFrame() throws IOException {
+        setTitle("认证 - 个人日志管理系统");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null); // 窗口居中
+        setLayout(new BorderLayout(10, 10)); // 使用BorderLayout
+
+        lblAvatar = new JLabel();
+        lblAvatar.setHorizontalAlignment(JLabel.CENTER);
+        lblAvatar.setBorder(new EmptyBorder(100, 0, 0, 0)); // 上边距为10像素
+
+        // 创建并添加头像标签到北区
+        BufferedImage originalImage = ImageIO.read(new File("src/main/java/resources/imges/avatar.jpg")); // 加载原始图片
+        // 调整头像大小
+        Image scaledImage = originalImage.getScaledInstance(150, 150, Image.SCALE_SMOOTH); // 缩放图片
+        lblAvatar.setIcon(new ImageIcon(scaledImage));
+        add(lblAvatar, BorderLayout.NORTH);
+
+        // 创建中央面板放置登录表单
+        JPanel centerPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        // 用户名输入框
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        centerPanel.add(new JLabel("Username:"), constraints);
+        txtUsername = new JTextField(20);
+        constraints.gridx = 1;
+        centerPanel.add(txtUsername, constraints);
+
+        // 密码输入框
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        centerPanel.add(new JLabel("Password:"), constraints);
+        txtPassword = new JPasswordField(20);
+        constraints.gridx = 1;
+        centerPanel.add(txtPassword, constraints);
+
+        // 登录按钮
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        btnLogin = new JButton("Login");
+        centerPanel.add(btnLogin, constraints);
+
+        add(centerPanel, BorderLayout.CENTER);
+
+        // 创建南区面板放置其他按钮
+        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        btnRegister = new JButton("Register");
+        btnReset = new JButton("Reset");
+        southPanel.add(btnRegister);
+        southPanel.add(btnReset);
+        add(southPanel, BorderLayout.SOUTH);
+
+        // 事件监听器
+        // TODO: 添加按钮的事件处理逻辑
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new LoginFrame().setVisible(true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
